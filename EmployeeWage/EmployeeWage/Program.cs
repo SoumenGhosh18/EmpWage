c706c9ff81644f0da55822a73e16328fef61c2ff
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-17 01:42:12 +0530

    Added: Daily employee wage functionality

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -8,10 +8,18 @@ namespace EmployeeWage
         const int IS_Full_TIME = 1;
         static void Main(string[] args)
         {
+            int EMP_RATE_PER_HOUR = 20;
+            int empwage = 0;
+            int emphrs = 8;
+
             Random r = new Random();
             int number = r.Next(2);
-            if ( number == IS_Full_TIME)
+            if (number == IS_Full_TIME)
+            {
                 Console.WriteLine("The employee is present");
+                empwage = emphrs* EMP_RATE_PER_HOUR;
+                Console.WriteLine("DAily EMPWAGE.......=" + empwage);
+            }
             else
                 Console.WriteLine("The employee is absent");
         }
