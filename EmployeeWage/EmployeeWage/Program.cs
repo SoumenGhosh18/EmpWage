da00e59f3d811d273ea3786c60ffe60a14b46f6a
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-17 01:54:41 +0530

    Added: Daily wage for the parttime employee

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -6,6 +6,7 @@ namespace EmployeeWage
     class Program
     {
         const int IS_Full_TIME = 1;
+        const int IS_PART_TIME = 2;
         static void Main(string[] args)
         {
             int EMP_RATE_PER_HOUR = 20;
@@ -13,12 +14,19 @@ namespace EmployeeWage
             int emphrs = 8;
 
             Random r = new Random();
-            int number = r.Next(2);
+            int number = r.Next(3);
             if (number == IS_Full_TIME)
             {
                 Console.WriteLine("The employee is present");
                 empwage = emphrs* EMP_RATE_PER_HOUR;
-                Console.WriteLine("DAily EMPWAGE.......=" + empwage);
+                Console.WriteLine("DAily fulltime empwage.......=" + empwage);
+            }
+            else if (number == IS_PART_TIME)
+            {
+                emphrs = 4;
+                empwage = emphrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Daily parttime empwage.......=" + empwage);
+
             }
             else
                 Console.WriteLine("The employee is absent");
