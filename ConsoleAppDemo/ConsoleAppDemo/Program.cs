a49f4be71333e2e4272241c2a4473f96487c0ea5
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-17 00:27:32 +0530

    Added: Functionality to check the employee is present or not

---
+++ ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the name........");
+           String s = Console.ReadLine();
+            Console.WriteLine(s);
+
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        const int IS_Full_TIME = 1;
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int number = r.Next(2);
+            if ( number == IS_Full_TIME)
+                Console.WriteLine("The employee is present");
+            else
+                Console.WriteLine("The employee is absent");
+        }
+    }
+}
